2463d57b24a5ebd76d2f4c1e9ff767554145ef0c
Mozzey <mbcacciano@gmail.com>
2018-08-08 20:57:28 -0400

    first commit

---
+++ Lab17.Library/PrimeIndexMatcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17.Library
+{
+    
+    public class PrimeIndexMatcher
+    {
+        public readonly List<int> PrimeNumbers;
+
+        public static int GetValue(int index)
+        {
+            int[] primes = new int[] { 2, 3 };
+            return primes[index - 1];
+        }
+    }
+}
---
+++ Lab17.Tests/PrimesIndexMatcherTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Lab17.Library.Tests
+{
+    [TestFixture]
+    public class PrimesIndexMatcherTests
+    {
+        [Test]
+        public void Index_Matcher_When1_Returns2()
+        {
+            // Assemble
+            int index = 1;
+            // Act
+            int output = PrimeIndexMatcher.GetValue(index);
+            // Assert
+            Assert.AreEqual(2, output);
+        }
+
+        [Test]
+        public void Index_Matcher_When2_Returns3()
+        {
+            // Assemble
+            int input = 2;
+            // Act
+            int output = PrimeIndexMatcher.GetValue(input);
+            // Assert
+            Assert.AreEqual(3, output);
+        }
+    }
+}
